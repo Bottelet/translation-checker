b61a6e18fceeee5d299db2ffe9779b5e03f83f74
Casper Bottelet <cbottelet@gmail.com>
2024-09-14 12:54:49 +0200

    refactor clean translation command to use base command

--- src/Commands/CleanTranslation.php
+++ src/Commands/CleanTranslation.php
@@ -6,10 +6,8 @@ use Bottelet\TranslationChecker\File\FileManagement;
 use Bottelet\TranslationChecker\File\Language\LanguageFileManager;
 use Bottelet\TranslationChecker\Finder\MissingKeysFinder;
 use Bottelet\TranslationChecker\Finder\TranslationFinder;
-use Illuminate\Console\Command;
-use RuntimeException;
 
-class CleanTranslation extends Command
+class CleanTranslation extends BaseTranslationCommand
 {
     protected $signature = 'translations:clean
                             {--source : The source language for the translations to clean}
@@ -21,30 +19,44 @@ class CleanTranslation extends Command
     {
         $this->info('Cleaning translations...');
 
-        $sourceLanguage = is_string($this->option('source')) ? $this->option('source') : 'en';
-        $sourceJsonPath = config('translator.language_folder') . "/{$sourceLanguage}.json";
-        $translationFinder = new TranslationFinder(new FileManagement(), new LanguageFileManager($sourceJsonPath), new MissingKeysFinder());
-        /** @var array<string> $sourceFilePaths */
-        $sourceFilePaths = config('translator.source_paths');
+        $options = $this->parseOptions();
+        $sourceJsonPath = $this->getTargetJsonPath($options->source);
+        $sourceFilePaths = $this->getSourceFilePaths();
 
+        $translationFinder = new TranslationFinder(
+            new FileManagement(),
+            new LanguageFileManager($sourceJsonPath),
+            new MissingKeysFinder()
+        );
 
         $missingTranslations = $translationFinder->findAllTranslations($sourceFilePaths);
         $sourceFileManager = new LanguageFileManager($sourceJsonPath);
         $sourceTranslations = $sourceFileManager->readFile();
 
-        foreach ($sourceTranslations as $key => $value) {
-            if (!in_array($key, $missingTranslations)) {
-                unset($sourceTranslations[$key]);
-            }
-        }
-
-        if ($this->option('print')) {
-            foreach ($sourceTranslations as $key => $value) {
-                $this->line("Key: {$key}, Value: {$value}");
-            }
+        $cleanedTranslations = array_intersect_key($sourceTranslations, array_flip($missingTranslations));
+        if ($options->print) {
+            $this->printTranslations($cleanedTranslations);
         } else {
-            $sourceFileManager->updateFile($sourceTranslations);
+            $sourceFileManager->updateFile($cleanedTranslations);
             $this->info('Unused translations removed from the source language file.');
+        }
+    }
+
+    protected function parseOptions(): CommandOptions
+    {
+        return new CommandOptions(
+            source: is_string($this->option('source')) ? $this->option('source') : 'en',
+            print: (bool) $this->option('print')
+        );
+    }
+
+    /**
+     * @param array<string, string> $translations
+     */
+    private function printTranslations(array $translations): void
+    {
+        foreach ($translations as $key => $value) {
+            $this->line("Key: {$key}, Value: {$value}");
         }
     }
 }
