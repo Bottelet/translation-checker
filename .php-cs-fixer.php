6b838987e242d794dae520c14a193b81d993cdad
Casper Bottelet <cbottelet@gmail.com>
2024-09-15 22:02:32 +0200

    update php-cs-fixer rules

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -11,4 +11,11 @@ $config = new PhpCsFixer\Config();
 return $config->setRules([
     '@PSR12' => true,
     'new_with_parentheses' => false,
+    'blank_line_after_namespace' => true,
+    'blank_line_after_opening_tag' => true,
+    'blank_line_before_statement' => [
+        'statements' => ['return'],
+    ],
+    'braces' => true,
+    'cast_spaces' => true,
 ])->setFinder($finder);
